2860dddc4d4d68ff462097f1a1f71a886a4d9d51
DOANANHQUAN <97490594+DOANANHQUAN@users.noreply.github.com>
2023-04-07 12:02:06 +0700

    Add files via upload

---
+++ DuoJs.js
@@ -0,0 +1,10 @@
+var Quest1_liketea = document.getElementById("Thiktra")
+var Quest1_hatetea = document.getElementById("Ghettra")
+function Quest1_right(){
+    alert("Chờ cập nhật câu 2:))")
+}
+function Sai(){
+    document.getElementById("Thua_Hiding").style.display = 'block'
+    document.getElementById("Quest1").style.display = 'none'
+    document.getElementById("Gioi_thieu").style.display = 'none'
+}
